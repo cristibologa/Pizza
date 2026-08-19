74d79c91b504fe38552a12d75c5ba10a218d0c48
Cristi <cristibologa98@yahoo.com>
2026-08-19 18:25:49 +0300

    add variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 function App() {
   return (
-    <div>
+    <div className="container">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
       delectus sed maiores ea, possimus autem officia sequi, aspernatur itaque
       facilis deleniti accusantium iusto ipsam sint nobis atque consequatur amet
       magnam?
+      <ul className="er">
+        <li className="asdf">fsd</li>
+      </ul>
     </div>
   );
 }
